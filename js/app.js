225ef462443afdeeae7f6ef39937d5ccfb93ca07
Mustafa Ali <mustafaaaly27@gmail.com>
2024-08-09 04:01:59 +0500

    login signup auth completed

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// import {
+//     app,db,collection, addDoc,getDocs ,auth, getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword 
+
+
+
+
+
+//   } from "../firebase.js"
+
+if(!localStorage.getItem("userId")){
+    window.location.replace("./pages/login.html")
+
+}
+
+
